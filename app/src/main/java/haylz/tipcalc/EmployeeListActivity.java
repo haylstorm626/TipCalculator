7200bf33b67c42eede84552d19e49766a2601266
haylstorm626 <keyb0ardf4ilure@gmail.com>
2018-05-28 00:56:55 -0400

    updated some layout features

--- app/src/main/java/haylz/tipcalc/EmployeeListActivity.java
+++ app/src/main/java/haylz/tipcalc/EmployeeListActivity.java
@@ -2,12 +2,22 @@ package haylz.tipcalc;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class EmployeeListActivity extends AppCompatActivity {
+
+    String[] tempTestArray = {"Mike","Aimee","Doug","Will"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_employee_list);
+
+        ArrayAdapter adapter = new ArrayAdapter<>(this, R.layout.employee_listview, tempTestArray);
+
+        ListView listView = findViewById(R.id.listEmployees);
+        listView.setAdapter(adapter);
+
     }
 }
--- app/src/main/java/haylz/tipcalc/MainActivity.java
+++ app/src/main/java/haylz/tipcalc/MainActivity.java
@@ -1,13 +1,34 @@
 package haylz.tipcalc;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button tip= findViewById(R.id.doTipsButton);
+        tip.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, DoTipsActivity.class));
+            }
+        });
+
+        Button emp= findViewById(R.id.employeeButton);
+        emp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, EmployeeListActivity.class));
+            }
+        });
     }
+
 }
