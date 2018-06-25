228bd80f4a58c1239d070c13041beaaca40b2ae8
haylstorm626 <keyb0ardf4ilure@gmail.com>
2018-06-25 13:48:14 -0400

    started do tips activity

--- app/src/main/java/haylz/tipcalc/DoTipsActivity.java
+++ app/src/main/java/haylz/tipcalc/DoTipsActivity.java
@@ -2,22 +2,42 @@ package haylz.tipcalc;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ListView;
+
+import java.util.List;
 
 public class DoTipsActivity extends AppCompatActivity {
 
-    String[] tempTestArray = {"Mike","Aimee","Doug","Will"};
+    String totMoney;
+    String totHours;
+    double[] hours;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_do_tips);
 
-        ArrayAdapter adapter = new ArrayAdapter<>(this, R.layout.employee_listview2, tempTestArray);
+        //input for total money
+        EditText totalMoney = (EditText)findViewById(R.id.totalMoney);
+        totMoney = totalMoney.getText().toString();
 
-        ListView listView = findViewById(R.id.listEmployees);
-        listView.setAdapter(adapter);
+        //input for total hours
+        EditText totalHours = (EditText)findViewById(R.id.totalHours);
+        totHours = totalHours.getText().toString();
 
+        //initialize employee list
+        EmployeeList list = EmployeeList.getInstance();
+        final ListView theListView = (ListView) findViewById(R.id.listEmployees);
+
+        View empty = findViewById(R.id.textNoneSaved);
+        theListView.setEmptyView(empty);
+
+        final List<Employee> dankList = list.load(getApplicationContext()); //= LOAD LIST
+
+        EmployeeListAdapter adapter = new EmployeeListAdapter(this, dankList);
+        theListView.setAdapter(adapter);
     }
 }
